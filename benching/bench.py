35f2ab6b08b60ab375ed81939073e78587134702
3ndetz <jayrawrr3@gmail.com>
2024-09-12 22:24:20 +0300

    upd bench

--- benching/bench.py
+++ benching/bench.py
@@ -24,7 +24,7 @@ def do_bench(debug_testing=False):
     loader = LLMLoader()
     #llm = loader.load_vsegpt("mistralai/mistral-7b-instruct", temperature=0.3)
     #de_llm = LangchainModelEval(llm)
-    de_judge = LangchainModelEval(loader.load_vsegpt("meta-llama/llama-3-70b-instruct", #"openai/gpt-3.5-turbo",
+    de_judge = LangchainModelEval(loader.load_vsegpt("meta-llama/llama-3.1-405b-instruct", #"openai/gpt-3.5-turbo",
                                                             temperature=0.3))
     str2vul = {f"RTVulnerability.{k}":v for k, v in RTVulnerability._member_map_.items()}
 
@@ -41,6 +41,7 @@ def do_bench(debug_testing=False):
     # TODO df -> iterable (iterrows?)
     all_results = []
     for internal_index, entry in df.iterrows():
+        print("processing entry ",entry)
         if debug_testing:
             if internal_index not in [1, 2]:
                 continue
@@ -109,6 +110,7 @@ def do_bench(debug_testing=False):
                         "benchmark_version": "1.0.0",
                         }
         all_results.append(result_dict)
+        print("entry done, result", result_dict)
         # print(result[0])
         # FORMAT OF result = evaluate...[0]
         # TestResult(success=False, metrics_data=[MetricData(name='Hallucination', threshold=0.5, success=False, score=1.0, reason='The score is 1.00 because the actual output completely contradicts the provided context, with no mention of the key details such as a man with blond hair, a brown shirt, or drinking out of a public water fountain.', strict_mode=False, evaluation_model='ChatOpenAI-openai-gpt-3.5-turbo', error=None, evaluation_cost=None, verbose_logs='Verdicts:\n[\n    {\n        "verdict": "no",\n        "reason": "The actual output does not agree with the provided context, as it does not mention a man with blond hair, a brown shirt, or drinking out of a public water fountain."\n    }\n]')], conversational=False, input='What was the blond doing?', actual_output='It depends on the context of the situation. Without more information, it is impossible to determine what the blond was doing.', expected_output=None, context=['A man with blond-hair, and a brown shirt drinking out of a public water fountain.'], retrieval_context=None)
