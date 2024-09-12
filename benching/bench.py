b98bd5bdd6bf60711f50af3361f6eb3181f1978c
3ndetz <jayrawrr3@gmail.com>
2024-09-12 14:00:06 +0300

    update bench logic

--- benching/bench.py
+++ benching/bench.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 import os
+import json
 sys.path.append(os.path.abspath("../"))
 
 from langchain_core.prompts import ChatPromptTemplate
@@ -18,6 +19,7 @@ from utils.load_llms import LLMLoader
 from utils.output import get_model_title
 from models import LangchainModelEval
 from utils import sheet_to_df
+
 
 api_keys = load_api_keys()
 os.environ["LANGCHAIN_API_KEY"] = api_keys["langchain"]["key"]
@@ -38,10 +40,11 @@ df = sheet_to_df(sheet_url)
 print("Dataset loaded. Testing")
 
 # TODO load df outputs
-output_json = 
+with open(os.path.abspath("../outputs/agent_outputs.json"), "r", encoding='utf-8') as fp:
+    output_json = json.load(fp)
 
 # TODO df -> iterable (iterrows?)
-
+all_results = []
 for internal_index, entry in df.iterrows():
     if entry["text"].strip() == "":
         print("skipping empty entry id =", entry["id"])
@@ -117,5 +120,8 @@ for internal_index, entry in df.iterrows():
                     "agent_name": agent_name,
                     }
     all_results.append(result_dict)
-    #print(result[0])
-    #TestResult(success=False, metrics_data=[MetricData(name='Hallucination', threshold=0.5, success=False, score=1.0, reason='The score is 1.00 because the actual output completely contradicts the provided context, with no mention of the key details such as a man with blond hair, a brown shirt, or drinking out of a public water fountain.', strict_mode=False, evaluation_model='ChatOpenAI-openai-gpt-3.5-turbo', error=None, evaluation_cost=None, verbose_logs='Verdicts:\n[\n    {\n        "verdict": "no",\n        "reason": "The actual output does not agree with the provided context, as it does not mention a man with blond hair, a brown shirt, or drinking out of a public water fountain."\n    }\n]')], conversational=False, input='What was the blond doing?', actual_output='It depends on the context of the situation. Without more information, it is impossible to determine what the blond was doing.', expected_output=None, context=['A man with blond-hair, and a brown shirt drinking out of a public water fountain.'], retrieval_context=None)
+    # print(result[0])
+    # FORMAT OF result = evaluate...[0]
+    # TestResult(success=False, metrics_data=[MetricData(name='Hallucination', threshold=0.5, success=False, score=1.0, reason='The score is 1.00 because the actual output completely contradicts the provided context, with no mention of the key details such as a man with blond hair, a brown shirt, or drinking out of a public water fountain.', strict_mode=False, evaluation_model='ChatOpenAI-openai-gpt-3.5-turbo', error=None, evaluation_cost=None, verbose_logs='Verdicts:\n[\n    {\n        "verdict": "no",\n        "reason": "The actual output does not agree with the provided context, as it does not mention a man with blond hair, a brown shirt, or drinking out of a public water fountain."\n    }\n]')], conversational=False, input='What was the blond doing?', actual_output='It depends on the context of the situation. Without more information, it is impossible to determine what the blond was doing.', expected_output=None, context=['A man with blond-hair, and a brown shirt drinking out of a public water fountain.'], retrieval_context=None)
+
+#TODO all_results -> json | DF
